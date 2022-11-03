380d70299f283e3695d147d15c73e0e0af8230b6
w68141 <yr.hvrn15@gmail.com>
2022-11-03 12:35:14 +0100

    7

---
+++ 7.py
@@ -0,0 +1,11 @@
+#for i in range(1, 101):
+#    print(i, end=' ')
+
+#for m in range(100, -1, -1):
+#    print(m, end=' ')
+
+#for n in range(7, 78):
+#    print(n, end=' ')
+
+for o in range(20, -1, -1):
+    print(o, end=' ')
